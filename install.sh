5ab2867cf4fed209532dd962a7114e920972bcf1
arrbegg <10772700+arrbegg@users.noreply.github.com>
2024-09-22 21:32:48 +0200

    Add Zsh config

--- install.sh
+++ install.sh
@@ -10,7 +10,6 @@ ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim"
 rm -rf "$HOME/.config/X11"
 ln -sf "$HOME/dotfiles/X11" "$HOME/.config"
 
-<<<<<<< HEAD
 ######
 # i3 #
 ######
@@ -21,12 +20,3 @@ ln -s "$HOME/dotfiles/i3" "$HOME/.config"
 mkdir -p "$HOME/.config/zsh"
 ln -sf "$HOME/dotfiles/zsh/.zshenv"  "$HOME"
 ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
-
-=======
-#######
-# i3  #
-#######
-
-rm -rf "$HOME/.config/i3"
-ln -sf "$HOME/dotfiles/i3" "$HOME/.config"
->>>>>>> 0e3d9e3e8632c44b91ded2080d5411415a15e244

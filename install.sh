6aa27e9719b91d525fec656531195de0ea56a3f8
arrbegg <10772700+arrbegg@users.noreply.github.com>
2024-09-23 19:04:53 +0200

    na dobrom putu

--- install.sh
+++ install.sh
@@ -3,12 +3,14 @@
 ########
 # nvim #
 ########
+
 mkdir -p "$HOME/.config/nvim"
 mkdir -p "$HOME/.config/nvim/undo"
+
 ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim"
 
 rm -rf "$HOME/.config/X11"
-ln -sf "$HOME/dotfiles/X11" "$HOME/.config"
+ln -s "$HOME/dotfiles/X11" "$HOME/.config"
 
 ######
 # i3 #
@@ -17,6 +19,13 @@ ln -sf "$HOME/dotfiles/X11" "$HOME/.config"
 rm -rf "$HOME/.config/i3"
 ln -s "$HOME/dotfiles/i3" "$HOME/.config"
 
+#######
+# Zsh #
+#######
+
 mkdir -p "$HOME/.config/zsh"
-ln -sf "$HOME/dotfiles/zsh/.zshenv"  "$HOME"
+ln -sf "$HOME/dotfiles/zsh/.zshenv" "$HOME"
 ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
+ln -sf "$HOME/dotfiles/zsh/aliases" "$HOME/.config/zsh/aliases"
+rm -rf "$HOME/.config/zsh/external"
+ln -sf "$HOME/dotfiles/zsh/external" "$HOME/.config/zsh"
